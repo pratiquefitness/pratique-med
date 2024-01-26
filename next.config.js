4e93f72069e0e7703b19fcf87d9d0abc4875a8f3
Wallysson <wlc.couto@gmail.com>
2024-01-25 21:47:39 -0300

    feat: adding vercel.json and configuring headers in next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,19 @@ const nextConfig = {
     RECAPCHA_CHECKOUT_KEY: process.env.RECAPCHA_CHECKOUT_KEY,
     RECAPCHA_CHECKOUT_SECRET: process.env.RECAPCHA_CHECKOUT_SECRET,
     API_KEY_PACTO: process.env.API_KEY_PACTO
+  },
+  async headers() {
+    return [
+      {
+        source: "/pages/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 }
 
