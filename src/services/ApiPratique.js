fe3bd7e82fb666247e9ee554e0b6f7d9e643c4db
pratiquefitness <pratiquefitness@gmail.com>
2024-01-29 17:43:08 -0300

    api

--- src/services/ApiPratique.js
+++ src/services/ApiPratique.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const ApiPratique = axios.create({
-  baseURL: `https://pratiquefitness.com.br`
+  baseURL: `https://med.pratiquefitness.com.br`
 })
 
 export default ApiPratique
